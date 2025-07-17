32f8bbaacedc61f184b9a824a4837bb366cd4e2d
passchaos <greedwolf.dss@gmail.com>
2025-07-17 20:59:33 +0800

    first commit

---
+++ dl-learn/playground.py
@@ -0,0 +1,19 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+x = np.arange(0, 6, 0.1)
+y = np.sin(x)
+y1 = np.cos(x)
+
+
+plt.plot(x, y, label = "sin")
+plt.plot(x, y1, linestyle = "--", label = "cos")
+
+plt.xlabel("x")
+plt.ylabel("y")
+
+plt.title("sin & cos")
+plt.legend()
+plt.show()
